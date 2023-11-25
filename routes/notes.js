232b2fbd27ea96e918abb4af12fdd8a4ba0ac0fd
Create-88 <141834742+Create-88@users.noreply.github.com>
2023-11-25 23:23:47 +1030

    WIP add delete

--- routes/notes.js
+++ routes/notes.js
@@ -3,6 +3,9 @@ const notes = require('express').Router();
 //Helper function to generate unique ids
 const uuid = require('../helpers/uuid');
 
+//import notesData from db.json
+const notesData = require('../db/db.json');
+
 //Helper function for reading and writing to the JSON file
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
 
@@ -45,16 +48,27 @@ notes.post('/', (req, res) => {
 
 //Write delete - not working yet
 
-notes.delete('/notes/:id', (request, response) => {
-    const noteId = Number(request.params.id);
-    const newNotes = notes.filter((note) => note.id != accountId);
+notes.delete('notes/:id', (req, res) => {
+    console.info(`${req.method} request received to delete note`);
+    //Log that a DELETE was received
+    const noteToDelete = parseString(req.params.id);
+    const updatedNotes = notesData.filter((id) => id !== noteToDelete);
   
-    if (!newNotes) {
+    if (!updatedNotes) {
       response.status(500).send('Note not found.');
     } else {
-      notes = newNotes;
-      response.send(notes);
+    response.status(200).send('Notes updated');
+      res.writeTotFile('./db/db.json', updatedNotes);
     }
 });
+
+// // Delete a task. Thanks to https://github.com/Evgen097/nodejs-test-project-examples/tree/99e9bb2c9bd9918ac4c4708b1923de4f424f68d0/10_Project_Karma/index.js#L53
+// notes.delete('/tasks/:id', function(req, res) {
+//     const id = req.params.id;
+//     app.db('tasks').remove({
+//       id: id
+//     });
+//     return res.status(201).end();
+//   });
 
 module.exports = notes;
