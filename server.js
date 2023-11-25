fe476ca15464d912023c4f4ceed913cd49962d18
Create-88 <141834742+Create-88@users.noreply.github.com>
2023-11-25 16:11:58 +1030

    change port

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
+//Import express.js
 const express = require('express');
+//path package brought in to resolve path of files that are located on server
 const path = require('path');
 
 //Import the notes router
 const api = require('./routes/index');
 
-const PORT = process.env.PORT || 3001;;
+//specify the port it will run on plus process.env port for heroku
+const PORT = process.env.PORT || 8080;
 
+//initialize an instance of express.js
 const app = express();
 
 //middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+//Middleware to serve up static assets from the public folder
 app.use(express.static('public'));
 
-//Middleware to serve up static assets from the public folder
 app.use ('/api', api);
 
 //GET route for homepage
-app.get('/', (req, res) =>
-res.sendFile(path.join(__dirname, 'public/index.html')));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
 
 //GET route for the notes page
 app.get('/notes', (req, res) => 
